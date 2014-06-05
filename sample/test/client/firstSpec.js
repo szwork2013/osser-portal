721cfc21bc67990bd8ca2d3eef0fca40413c6bbd
Koma <admin@osser.jp>
2014-06-05 10:37:45 +0900

    updated sample project for karma

---
+++ sample/test/client/firstSpec.js
@@ -0,0 +1,11 @@
+describe('first tset', function () {
+    it('test case1', function () {
+        console.log('test case1');
+    });
+    it('test case2', function () {
+        console.info('test case2');
+    });
+    it('test case3', function () {
+        console.error('test case3');
+    });
+});
